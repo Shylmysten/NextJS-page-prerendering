c477f7afa858d50feaf69c52a6ef7f7c69f67713
Tony Boswell <tboswell@imodules.com>
2023-11-29 17:05:21 -0600

    2 other getStaticProps configuration properties

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,26 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticProps() {
-  console.log("Regenerating...")
+export async function getStaticProps(context) {
+  console.log("Regenerating...");
   // all code here executes on the server side
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+  
+  // if no data found can redirect the user to another path/page
+  if(!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      }  
+    }
+  }
+
+  if(data.products.length === 0) {
+     // set to true renders a 404 error page instead of the normal page if no product(s) are found
+    return { notFound: true };
+  }
 
   return { 
     props: {
@@ -24,6 +38,8 @@ export async function getStaticProps() {
     },
     // regenerate this static page every 60 seconds
     revalidate: 60,
+    // set to true renders a 404 error page instead of the normal page
+    //notFound: true,
   };
 }
 
